771400a98775000329e098be32d41208f8218abf
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2018-06-03 22:53:27 +0900

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import * as Express from 'express';
+
+const app = Express();
+
+app.get(
+  '/',
+  (req: Express.Request, res: Express.Response) => {
+      return res.send('Hello world');
+  },
+);
+
+const port = 3000;
+app.listen(
+  port,
+  () => {
+      console.log(`Example app listening on port ${port}`);
+  },
+);
+
+export default app;
