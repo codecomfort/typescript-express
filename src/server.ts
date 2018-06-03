e38ccc9b9c514521229a9bb2160259488493696f
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2018-06-03 23:37:16 +0900

    [add] tslint-plugin-prettier

--- src/server.ts
+++ src/server.ts
@@ -2,20 +2,14 @@ import * as Express from 'express';
 
 const app = Express();
 
-app.get(
-  '/',
-  (req: Express.Request, res: Express.Response) => {
-      return res.send('Hello world');
-  },
-);
+app.get('/', (req: Express.Request, res: Express.Response) => {
+  return res.send('Hello world');
+});
 
 const port = 3000;
 
-app.listen(
-  port,
-  () => {
-      console.log(`Example app listening on port ${port}`);
-  },
-);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
 export default app;
