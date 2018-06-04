b70912b0554be7fa228ab75c9de5d91811053a99
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2018-06-04 12:08:41 +0900

    [refactor] logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,16 @@
 import { RequestHandler } from 'express';
 
 const logger: RequestHandler = (req, res, next) => {
-  console.log('output from logger');
+  const ip =
+    req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    (req.socket && req.socket.remoteAddress) ||
+    '0.0.0.0';
+  const date = new Date().toISOString();
+
+  console.log(
+    `${ip} [${date}] "${req.method} ${req.url} - ${req.headers['user-agent']}"`
+  );
   next();
 };
 
