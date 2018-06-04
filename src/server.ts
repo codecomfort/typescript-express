1d0a2d10ace17c8c4f844f9add92a242705e39ca
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2018-06-04 12:14:52 +0900

    [add] simple routing

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,12 @@ import logger from './logger';
 const app = Express();
 app.use(logger);
 
+app.get('/home/index', (req, res) => {
+  res.status(200).send('OK');
+});
+
 app.get('/', (req: Express.Request, res: Express.Response) => {
-  return res.send('Hello world');
+  res.send('Hello world');
 });
 
 const port = 3000;
